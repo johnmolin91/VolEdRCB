a9be077174b6d0340e634e5f805167f8fa99ba20
johnmolin91 <john.molin91@gmail.com>
2018-03-13 17:57:54 -0400

    index

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -20,6 +20,6 @@ module.exports = function(app) {
   });
 
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index1.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 };
